a1349135546a3bcc59e3684d4d68d7926d3160f6
Malcolm Gibbons <malcolm@texel.com.ar>
2022-11-09 19:19:08 -0300

    Arreglo de paginas JS

--- js/app.js
+++ js/app.js
@@ -12,18 +12,29 @@ const checkBoxes = () => {
 window.addEventListener("scroll", checkBoxes);
 checkBoxes()
 
-let paginaElegida = "Hidrogenos"
+let paginaElegida
 
-window.addEventListener('load', () => {
+
+if(localStorage.getItem("paginaElegida") == null){
+    paginaElegida = "Hidrogenos"
+} else {
     paginaElegida = localStorage.getItem("paginaElegida")
-})
-
-const tipoDePagina = (tipo) => {
-    paginaElegida = tipo
-    localStorage.setItem("paginaElegida", paginaElegida);
 }
 
-const irA = (pagina) => {
-    window.open(`./Page ${paginaElegida}/${pagina}.html`, "_self");
+
+const tipoDePagina = (atributo, tipo) => {
+    localStorage.setItem(atributo, tipo);
+}
+
+const irA = (pagina, idPagina, tipoDeRecurso) => {
+
+    if (idPagina == -1) {
+        window.open(`./Page ${paginaElegida}/${pagina}.html`, "_self");
+    } else if (paginaElegida == "Renovables") {
+        window.open(`./Page ${paginaElegida}${tipoDeRecurso+pagina}.html`, "_self");
+    } else if (paginaElegida == "Hidrogenos"){
+        window.open(`./Page ${paginaElegida}${tipoDeRecurso+pagina}.html`, "_self");
+    }
+
 }
 
